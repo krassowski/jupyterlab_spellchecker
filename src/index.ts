eff405a3c6eded7d08590c3d5936dc3b036d4a4f
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2026-01-21 11:40:06 +0000

    Add null check for `editorView`

--- src/index.ts
+++ src/index.ts
@@ -487,6 +487,9 @@ class SpellChecker {
       return null;
     }
     const editorView = EditorView.findFromDOM(code_mirror_wrapper);
+    if (!editorView) {
+      return null;
+    }
     const offset = editorView.posAtCoords({
       x: event.clientX,
       y: event.clientY
